ffd5ebbe2ac843a9fcfc4bc0e87b9ccdbf41e2d6
Andres Valencia <andresvalencia7270@gmail.com>
2023-05-26 19:23:14 +0200

    const, final y late

--- dart/03_finalconst.dart
+++ dart/03_finalconst.dart
@@ -6,11 +6,28 @@ main() {
   // b = 20; más ligera cuando no se va a cambiar el valor de la variable
   // c = 20; jamas se va a cambiar el valor del objeto
 
-  final personasFinal = ['Juan', 'Pedro', 'Fernando'];
+  final List<String> personasFinal = ['Juan', 'Pedro', 'Fernando'];
   personasFinal.add('Felipe');
-  const personasConst = ['Juan', 'Pedro', 'Fernando'];
-  personasConst.add('Felipe no se agrego'); //Cannot add to an unmodifiable list
+  const List<String> personasConst = ['Juan', 'Pedro', 'Fernando'];
+  /* 
+    List<String> personasConst = const['Juan', 'Pedro', 'Fernando'];
+  */
+  personasConst.add('Felipe no se agrego');
+  //Cannot add to an unmodifiable list
 
-  print(personasFinal);
+  //print(personasFinal);
+
+  /*personasFinal =
+  []; //apunta a un nuevo espacio en memoria no es valido gracias a final*/
   // print(personasConst);
+
+  // late permite inicializar una variable después
+
+  // double x = 10.25;
+  late final double x;
+  //const en tiempo de compilación más ligera
+
+  x = 2.0;
+
+  print(x);
 }
