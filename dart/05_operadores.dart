f67806cd579994765c1316b1f02c73dc51c07b14
Andres Valencia <andresvalencia7270@gmail.com>
2023-05-26 19:40:14 +0200

    operadores matemáticos

--- dart/05_operadores.dart
+++ dart/05_operadores.dart
@@ -1,1 +1,16 @@
+/**
+ * un operador es un símbolo que le dice al compilador
+ *  qué debe de realizar una tarea
+ *  matemática, relacional o lógica
+ * y debe de producir un resultado
+ */
 
+main() {
+  // +
+  // -
+  // '*'
+  // %
+  // ~/ división común y corriente pero solo la parte entera de la división
+  // ++
+  // += número, *=número, /=número, etc
+}
