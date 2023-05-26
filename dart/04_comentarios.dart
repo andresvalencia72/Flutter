a6dbd5bfb9633b3452a46a9a313b3e8f4356584d
Andres Valencia <andresvalencia7270@gmail.com>
2023-05-26 19:33:55 +0200

    comentarios y documentacion con dartdoc

---
+++ dart/04_comentarios.dart
@@ -0,0 +1,24 @@
+/**
+   * este archivo tiene por objetivo los comentarios en dart
+   * dartdoc
+   */
+main() {
+  // este es un comentario de una línea
+  int a = 10;
+
+  final personas = [
+    // primera persona
+    'Fernando',
+    // segunda persona
+    'Perdro',
+    // tercera persona
+    'Carlos',
+  ];
+
+  // comentario de bloque
+  /* esto es un comentario de bloque */
+
+  ///Funciona para saludar
+  ///Recibe un [nombre] y se concatena con el [mensaje]
+  saludar(String nombre, String mesaje) {}
+}
