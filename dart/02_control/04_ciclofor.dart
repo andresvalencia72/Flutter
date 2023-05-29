7c9a1a78f9681133e5cdd6feec4b1214fdb1656c
Andres Valencia <andresvalencia7270@gmail.com>
2023-05-29 16:41:50 +0200

    ciclo for in

--- dart/02_control/04_ciclofor.dart
+++ dart/02_control/04_ciclofor.dart
@@ -3,4 +3,12 @@ main() {
   for (int i = 0; i < 10; i++) {
     print('i = $i');
   }
+
+  /* ciclo for in */
+  List<String> listado = ['huevos', 'leche', 'pan'];
+  listado.add('carne');
+
+  for (String prod in listado) {
+    print('Comprar: ' + prod);
+  }
 }
